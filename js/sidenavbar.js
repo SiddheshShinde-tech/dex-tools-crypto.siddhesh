d3f78ba935d835e3dbc4c4ba158df18c19e1f340
SiddheshShinde-tech <siddhesh.shinde19@st.niituniversity.in>
2021-06-16 14:12:17 +0530

    Initial file setup and sidenavbar

---
+++ js/sidenavbar.js
@@ -0,0 +1,8 @@
+// Functions for sidebar
+function sidebarClose() {
+  document.querySelector(".sidebar").classList.add("close");
+}
+
+function sidebarOpen() {
+  document.querySelector(".sidebar").classList.remove("close");
+}
